ee7e20442e7be67f52df348ef14eae38855ed8e5
Anuj Kumar <anuj.kumar@mindmapdigital.ai>
2026-04-06 17:06:46 +0530

    made the setup excutable

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ if [ -d "$AGENTOS_DIR" ]; then
     echo "Directory '$AGENTOS_DIR' exists. Deleting the Directory"
     rm -rf "$AGENTOS_DIR" || { echo "Failed to delete the directory."; exit 1; }
     echo "Directory '$AGENTOS_DIR' deleted."
+else
+    echo "Directory '$AGENTOS_DIR' does not exist."
 fi
-echo "Directory '$AGENTOS_DIR' does not exist."
 mkdir -p "$AGENTOS_DIR" || { echo "Failed to create the directory."; exit 1; }
 echo "Directory '$AGENTOS_DIR' created."
 cp -rf agentos/* "$AGENTOS_DIR" || { echo "Failed to copy the files."; exit 1; }
@@ -21,19 +22,22 @@ cp -rf system/* "$SYSTEMD_DIR" || { echo "Failed to copy the directory."; exit 1
 ## 3. making files executable
 sudo chmod +x "$AGENTOS_DIR"/*
 
+## installing required libraries
+# sudo 
+
 ## 4. tell systemd to reload - it needs to see the new files
 sudo systemctl daemon-reload
 
-## 5. start the service
-sudo systemctl start agentd
+## 5. enable to start on everyboot
+sudo systemctl enable agentd
 
-## 6. check the status of the service
+## 6. start the service
+sudo systemctl start agentd
+ 
+## 7. check the status of the service
 sudo systemctl status agentd
 
-## 7. watch the live logs
-sudo journalctl -f -u agentd -f
-
-## 8. enable to start on everyboot
-sudo systemctl enable agentd
+## 8. watch the live logs (Final blocking step)
+sudo journalctl -u agentd -f
 
 ##
