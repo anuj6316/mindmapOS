b3e9f91a4b515bae393347a00d1578be072fc12f
Anuj Kumar <anuj.kumar@mindmapdigital.ai>
2026-04-06 16:37:40 +0530

    restructured the codebase

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ fi
 echo "Directory '$AGENTOS_DIR' does not exist."
 mkdir -p "$AGENTOS_DIR" || { echo "Failed to create the directory."; exit 1; }
 echo "Directory '$AGENTOS_DIR' created."
-cp -rf "agentos/*" "$AGENTOS_DIR" || { echo "Failed to copy the files."; exit 1; }
+cp -rf agentos/* "$AGENTOS_DIR" || { echo "Failed to copy the files."; exit 1; }
 echo "Files copied to '$AGENTOS_DIR'."
 
 # systemd dir
 SYSTEMD_DIR="/lib/systemd/system"
-cp -rf "system/*" "$SYSTEMD_DIR" || { echo "Failed to copy the directory."; exit 1; }
+cp -rf system/* "$SYSTEMD_DIR" || { echo "Failed to copy the directory."; exit 1; }
