d8ae14fcb9e6821c051f3c63c1889f14c624b087
Anuj Kumar <anuj.kumar@mindmapdigital.ai>
2026-04-06 16:44:30 +0530

    udpated the setup script to enable the service on everyboot

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+## 1. setting up the agentos
 AGENTOS_DIR="/opt/agentos"
 
 # Check if directory exists
@@ -12,6 +13,27 @@ echo "Directory '$AGENTOS_DIR' created."
 cp -rf agentos/* "$AGENTOS_DIR" || { echo "Failed to copy the files."; exit 1; }
 echo "Files copied to '$AGENTOS_DIR'."
 
-# systemd dir
+
+## 2. setting up systemd script
 SYSTEMD_DIR="/lib/systemd/system"
 cp -rf system/* "$SYSTEMD_DIR" || { echo "Failed to copy the directory."; exit 1; }
+
+## 3. making files executable
+sudo chmod +x "$AGENTOS_DIR"/*
+
+## 4. tell systemd to reload - it needs to see the new files
+sudo systemctl daemon-reload
+
+## 5. start the service
+sudo systemctl start agentd
+
+## 6. check the status of the service
+sudo systemctl status agentd
+
+## 7. watch the live logs
+sudo journalctl -f -u agentd -f
+
+## 8. enable to start on everyboot
+sudo systemctl enable agentd
+
+##
